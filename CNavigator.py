12da2983bd0e748ef046cbd69db11f3f5fbba88a
yu ge <yuge375841@ruc.edu.cn>
2024-11-14 21:12:22 -0500

    test3

--- CNavigator.py
+++ CNavigator.py
@@ -14,6 +14,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
 
+
 os.environ["OPENAI_API_KEY"] = "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"
 
 llm = ChatOpenAI(model="gpt-4o-mini")
