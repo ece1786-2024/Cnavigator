0c4936f50608ceb215d08495f701b7b75aed4a21
yu ge <yuge375841@ruc.edu.cn>
2024-11-15 19:58:05 -0500

    remove old version

--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import datetime
-
-def create_log_directory():
-    log_dir = "conversation_logs"
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    return log_dir
